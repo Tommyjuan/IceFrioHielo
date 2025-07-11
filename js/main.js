13ab3fb9800ab389b33e05d8fcc48d19770056a9
Juan José <juan.colorado89@correo.tdea.edu.co>
2025-07-11 17:38:21 -0500

    estilo contacto, logo index, pobtón hamburguesa

--- js/main.js
+++ js/main.js
@@ -18,11 +18,11 @@ function includeHTML(id, file) {
 
 // Ejecutar todo cuando cargue el DOM
 window.addEventListener('DOMContentLoaded', () => {
-  // ✅ Cargar cabecera y pie
+  // Cargar cabecera y pie
   includeHTML('#cabecera', '../template/cabecera.html');
   includeHTML('#pie', '../template/pie.html');
 
-  // ✅ Carrusel automático
+  // Carrusel automático
   const carruselInner = document.querySelector('.carrusel-inner');
   const carruselItems = document.querySelectorAll('.carrusel-item');
   let currentIndex = 0;
@@ -33,6 +33,19 @@ window.addEventListener('DOMContentLoaded', () => {
       carruselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
     }
 
-    setInterval(cambiarImagen, 3000); // cada 5 segundos
+    setInterval(cambiarImagen, 4000); 
   }
 });
+
+// botón tipo hamburguesa del header
+ document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.querySelector('.navbar-toggler');
+    const searchForm = document.querySelector('.search-form');
+
+    toggleButton.addEventListener('click', function () {
+      // Solo se aplica en pantallas pequeñas
+      if (window.innerWidth < 992) {
+        searchForm.classList.toggle('d-none');
+      }
+    });
+  });
